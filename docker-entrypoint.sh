23025b99721186b1863345ee616dad43ac0a1d18
Mohammad Amin <dadgaramin96@gmail.com>
2024-11-25 17:52:00 +0330

    fix: coverage output file!

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 python3 -m coverage run --omit=tests/* -m pytest . && echo "Tests Passed" || exit 1
-
-cp .coverage ../.coverage
-cd .. || exit
-python3 -m coverage lcov -i -o coverage/lcov.info
+python3 -m coverage lcov -o coverage/lcov.info
